1cb6035df341d4eb83ab0fee49964c963d48965c
oktay <zemisty@hotmail.com>
2023-08-09 21:36:57 +0200

    laatste update

---
+++ cmake-build-debug/CMakeFiles/schaken.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/cosku/CLionProjects/Schaaktaak/SchaakGUI.cpp" "CMakeFiles/schaken.dir/SchaakGUI.cpp.obj" "gcc" "CMakeFiles/schaken.dir/SchaakGUI.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/SchaakStuk.cpp" "CMakeFiles/schaken.dir/SchaakStuk.cpp.obj" "gcc" "CMakeFiles/schaken.dir/SchaakStuk.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/game.cpp" "CMakeFiles/schaken.dir/game.cpp.obj" "gcc" "CMakeFiles/schaken.dir/game.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/guicode/ChessWindow.cpp" "CMakeFiles/schaken.dir/guicode/ChessWindow.cpp.obj" "gcc" "CMakeFiles/schaken.dir/guicode/ChessWindow.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/guicode/chessboard.cpp" "CMakeFiles/schaken.dir/guicode/chessboard.cpp.obj" "gcc" "CMakeFiles/schaken.dir/guicode/chessboard.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/guicode/fileIO.cpp" "CMakeFiles/schaken.dir/guicode/fileIO.cpp.obj" "gcc" "CMakeFiles/schaken.dir/guicode/fileIO.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/guicode/message.cpp" "CMakeFiles/schaken.dir/guicode/message.cpp.obj" "gcc" "CMakeFiles/schaken.dir/guicode/message.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/main.cpp" "CMakeFiles/schaken.dir/main.cpp.obj" "gcc" "CMakeFiles/schaken.dir/main.cpp.obj.d"
+  "C:/Users/cosku/CLionProjects/Schaaktaak/cmake-build-debug/schaken_autogen/mocs_compilation.cpp" "CMakeFiles/schaken.dir/schaken_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/schaken.dir/schaken_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
